f7df08b351f02d1d8bf0dab367bc87e852df5d85
Nanashibi <adithyasubodh8@gmail.com>
2025-04-15 21:59:50 +0530

    Added global variable

--- frontend/src/components/NFTDetails.js
+++ frontend/src/components/NFTDetails.js
@@ -1,3 +1,5 @@
+/* global BigInt */
+
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Container, Row, Col, Card, Button, Spinner, Alert } from 'react-bootstrap';
